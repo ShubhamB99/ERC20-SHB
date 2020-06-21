672d98031c377829850de506cb3bba7e6b459a03
Shubham Bhardwaj <shubhambhardwaj334@gmail.com>
2020-06-21 22:42:26 +0530

    Deploying on Goerli and creating Uniswap Pool

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ERC20-SHB
 This repository contains code for creation of ERC20 token and deploying on Goerli
+
+* **SHB Token Address** - 0x7E018b49ac797303368138a0854BE52abBA147e8
+* **Uniswap Pool Address** - 0x8338be66EA74E601fde0A1638694b63C3a296a75
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -46,6 +50,16 @@ module.exports = {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
+    },
+
+    goerli: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, 'https://goerli.infura.io/v3/381549724f704aee9a09b24a7033f368', 0)
+      },
+      network_id: '5', // eslint-disable-line camelcase
+      gas: 4465030,
+      gasPrice: 10000000000,
+      timeoutBlocks: 200
     },
 
     // Another network with more advanced options...
